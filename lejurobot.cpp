8b6e6fd2a4f16e546da61c2053edf44844ce1755
chenhuaming <337467729@qq.com>
2024-04-23 17:41:45 +0800

    lejurobot update

--- lejurobot.cpp
+++ lejurobot.cpp
@@ -2,7 +2,83 @@
 #include "WalkPatternGen.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <cmath>
 
+template <typename scalar>
+constexpr scalar ANGLE_OFFSET = 100;
+
+constexpr size_t SPEED_DEFAULT = 40;
+constexpr u_short STIFFNESS_DEFAULT = 40;
+
+// generate leju robot IDE happy angle string
+template <typename scalar>
+std::string GenerateFormatAngle(const Eigen::Vector<scalar, 5> &right_leg, const Eigen::Vector<scalar, 5> &left_leg,
+                                const Eigen::Vector<scalar, 3> &right_hand, const Eigen::Vector<scalar, 3> &left_hand,
+                                const Eigen::Vector<scalar, 3> &remote_motor, const size_t speed = SPEED_DEFAULT)
+{
+  std::ostringstream oss;
+  scalar angle_offset = ANGLE_OFFSET<scalar>;
+  oss << "SPEED " << speed << std::endl;
+  oss << "MOTORA," << round(left_leg(4) + angle_offset) << "," << round(left_leg(3) + angle_offset) << "," << round(left_leg(2) + angle_offset) << "," << round(left_leg(1) + angle_offset) << "," << round(left_leg(0) + angle_offset) << std::endl;
+  oss << "MOTORB," << round(right_leg(4) + angle_offset) << "," << round(right_leg(3) + angle_offset) << "," << round(right_leg(2) + angle_offset) << "," << round(right_leg(1) + angle_offset) << "," << round(right_leg(0) + angle_offset) << std::endl;
+  oss << "MOTORC," << round(left_hand(2) + angle_offset) << "," << round(left_hand(1) + angle_offset) << "," << round(left_hand(0) + angle_offset) << std::endl;
+  oss << "MOTORD," << round(right_hand(2) + angle_offset) << "," << round(right_hand(1) + angle_offset) << "," << round(right_hand(0) + angle_offset) << std::endl;
+  oss << "MOTORE," << round(remote_motor(2) + angle_offset) << "," << round(remote_motor(1) + angle_offset) << "," << round(remote_motor(0) + angle_offset) << std::endl;
+  oss << "WAIT" << std::endl;
+  return oss.str();
+}
+
+// generate leju robot IDE happy stiffness string
+std::string GenerateFormatStiffness(const Eigen::Vector<u_short, 5> &right_leg, const Eigen::Vector<u_short, 5> &left_leg,
+                                    const Eigen::Vector<u_short, 3> &right_hand, const Eigen::Vector<u_short, 3> &left_hand,
+                                    const Eigen::Vector<u_short, 3> &remote_motor)
+{
+  std::ostringstream oss;
+  oss << "RIGIDA," << left_leg(4) << "," << left_leg(3) << "," << left_leg(2) << "," << left_leg(1) << "," << left_leg(0) << std::endl;
+  oss << "RIGIDB," << right_leg(4) << "," << right_leg(3) << "," << right_leg(2) << "," << right_leg(1) << "," << right_leg(0) << std::endl;
+  oss << "RIGIDC," << left_hand(2) << "," << left_hand(1) << "," << left_hand(0) << std::endl;
+  oss << "RIGIDC," << right_hand(2) << "," << right_hand << "," << right_hand(0) << std::endl;
+  oss << "RIGIDE," << remote_motor(2) << "," << remote_motor(1) << "," << remote_motor(0) << std::endl;
+  oss << "RIGEND" << std::endl;
+  return oss.str();
+}
+
+template <typename ios, typename scalar>
+void GenerateFormatBodyToFile(ios &os,
+                              const std::vector<Eigen::Vector<scalar, 12>> &leg_angle, const std::vector<Eigen::Vector<scalar, 3>> &right_hand,
+                              const std::vector<Eigen::Vector<scalar, 3>> &left_hand, const std::vector<Eigen::Vector<scalar, 3>> &remote_motor)
+{
+  static_assert(std::is_base_of_v<std::iostream, os> == true);
+  assert(leg_angle.size() == left_hand.size());
+  assert(leg_angle.size() == right_hand.size());
+  assert(leg_angle.size() == remote_motor.size());
+  const Eigen::Vector<u_short, 5> &stiffness_leg = Eigen::Vector<u_short, 5>::Constant(STIFFNESS_DEFAULT);
+  const Eigen::Vector<u_short, 3> &stiffness_hand = Eigen::Vector<u_short, 3>::Constant(STIFFNESS_DEFAULT);
+  os << GenerateFormatStiffness(stiffness_leg, stiffness_leg, stiffness_hand, stiffness_hand, stiffness_hand);
+  for (size_t i = 0; i < leg_angle.size(); ++i)
+  {
+    os << GenerateFormatAngle(leg_angle[i].template segment<5>(1), leg_angle[i].template segment<5>(7), right_hand[i], left_hand[i], remote_motor[i]);
+  }
+  return;
+}
+// only generate walk, keep other motor as before
+template <typename ios, typename scalar>
+void GenerateFormatWalkToFile(ios &os,
+                              const std::vector<Eigen::Vector<scalar, 12>> &leg_angle, const Eigen::Vector<scalar, 3> &right_hand,
+                              const Eigen::Vector<scalar, 3> &left_hand, const Eigen::Vector<scalar, 3> &remote_motor)
+{
+  static_assert(std::is_base_of_v<std::iostream, os> == true);
+  const Eigen::Vector<u_short, 5> &stiffness_leg = Eigen::Vector<u_short, 5>::Constant(STIFFNESS_DEFAULT);
+  const Eigen::Vector<u_short, 3> &stiffness_hand = Eigen::Vector<u_short, 3>::Constant(STIFFNESS_DEFAULT);
+  os << GenerateFormatStiffness(stiffness_leg, stiffness_leg, stiffness_hand, stiffness_hand, stiffness_hand);
+  for (size_t i = 0; i < leg_angle.size(); ++i)
+  {
+    os << GenerateFormatAngle(leg_angle[i].template segment<5>(1), leg_angle[i].template segment<5>(7), right_hand, left_hand, remote_motor);
+  }
+  return;
+}
 int main()
 {
   WalkPatternGen<double> walk1;
@@ -10,6 +86,6 @@ int main()
   walk1.GenerateStillStep(WalkPatternGen<double>::Tstep);
   walk1.UpdateState();
   walk1.GenerateTrajectoryPosition();
-  
+
   return 0;
 }
